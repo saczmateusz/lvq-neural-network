8115a0cf9563b1133475693c1d7b896688217613
Mateusz Sączawa <sacz.mateusz@gmail.com>
2019-05-21 08:09:54 +0200

    Update plot2d.py

--- plot2d.py
+++ plot2d.py
@@ -5,17 +5,17 @@ import matplotlib.pyplot as plt
 x_n = list()
 y_p = list()
 
-with open('neurons10-300_lr_03_ep_100/neuron_test_10-200_2_cb.txt', 'r') as xf:
+with open('epoch_10-100_nv_90-100_lr_03/epoch_10-150_nv_90_lr_03_2_ep.txt', 'r') as xf:
     for x in xf:
         x_n.append(int(x))
 
-with open('neurons10-300_lr_03_ep_100/neuron_test_10-200_3_pk.txt', 'r') as yf:
+with open('epoch_10-100_nv_90-100_lr_03/epoch_10-150_nv_90_lr_03_3_pk.txt', 'r') as yf:
     for y in yf:
         y_p.append(float(y))
 
 plt.plot(x_n, y_p, 'r')
-plt.xlabel('Liczba neuronów')
-plt.ylabel('Poprawność klasyfikacji [%]', rotation='vertical')
-plt.axis([10, 300, 90, 100])
+plt.xlabel('Epoki')
+plt.ylabel('PK [%]', rotation='vertical')
+plt.axis([10, 150, 90, 100])
 plt.grid(True)
 plt.show()
